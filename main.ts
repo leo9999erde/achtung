8bb97af6916ef02b9239fa3c70f842704f7ecf3b
leo9999erde <96633138+leo9999erde@users.noreply.github.com>
2022-07-16 15:55:00 +0200

    verschidene werscheinlichkeiten

--- main.ts
+++ main.ts
@@ -9,6 +9,12 @@ input.onButtonPressed(Button.A, function () {
             `)
         basic.setLedColor(0xff0000)
         music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playTone(523, music.beat(BeatFraction.Whole))
     } else {
         basic.showLeds(`
             . . . . .
@@ -19,7 +25,76 @@ input.onButtonPressed(Button.A, function () {
             `)
         basic.setLedColor(0x00ff00)
         music.playMelody("E B C5 A B G A F ", 120)
-        music.playMelody("- - - - - - - - ", 120)
+        music.playMelody("E D G F B A C5 B ", 120)
+        music.playMelody("E B C5 A B G A F ", 120)
+        music.playMelody("E D G F B A C5 B ", 120)
+        music.playMelody("E B C5 A B G A F ", 120)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (randint(1, 2) == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.setLedColor(0xff0000)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playTone(523, music.beat(BeatFraction.Whole))
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+        basic.setLedColor(0x00ff00)
+        music.playMelody("E B C5 A B G A F ", 120)
+        music.playMelody("E D G F B A C5 B ", 120)
+        music.playMelody("E B C5 A B G A F ", 120)
+        music.playMelody("E D G F B A C5 B ", 120)
+        music.playMelody("E B C5 A B G A F ", 120)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (randint(1, 100) == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.setLedColor(0xff0000)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playMelody("C - C - C - D D ", 200)
+        music.playTone(523, music.beat(BeatFraction.Whole))
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+        basic.setLedColor(0x00ff00)
+        music.playMelody("E B C5 A B G A F ", 120)
+        music.playMelody("E D G F B A C5 B ", 120)
+        music.playMelody("E B C5 A B G A F ", 120)
+        music.playMelody("E D G F B A C5 B ", 120)
+        music.playMelody("E B C5 A B G A F ", 120)
     }
 })
 basic.forever(function () {
