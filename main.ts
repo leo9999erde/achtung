5ceb1f4df6e523173131bf9523755072f115a819
leo9999erde <96633138+leo9999erde@users.noreply.github.com>
2022-07-16 16:30:52 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,117 @@
+input.onPinPressed(TouchPin.P0, function () {
+    if (true) {
+        if (randint(1, 10) == 2) {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+            basic.setLedColor(0xff0000)
+            music.playMelody("C - C - C - D D ", 200)
+            music.playTone(523, music.beat(BeatFraction.Breve))
+            basic.showLeds(`
+                # # # # #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                . # . # .
+                # . . . #
+                `)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # . . . #
+                `)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        } else {
+            basic.showLeds(`
+                . . . . .
+                . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                `)
+            basic.setLedColor(0x00ff00)
+            music.playMelody("E B C5 A B G A F ", 120)
+            music.playMelody("E D G F B A C5 B ", 120)
+            basic.showLeds(`
+                # # # # #
+                . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                `)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                . . . # .
+                # . # . .
+                . # . . .
+                `)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # . # . .
+                . # . . .
+                `)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                . # . . .
+                `)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+        basic.turnRgbLedOff()
+    }
+})
 input.onButtonPressed(Button.A, function () {
-    if (randint(1, 10) == 2) {
+    if (randint(1, 100) == 2) {
         basic.showLeds(`
             # . . . #
             . # . # .
@@ -109,117 +221,10 @@ input.onButtonPressed(Button.A, function () {
     basic.turnRgbLedOff()
 })
 input.onButtonPressed(Button.AB, function () {
-    if (randint(1, 1) == 2) {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        basic.setLedColor(0xff0000)
-        music.playMelody("C - C - C - D D ", 200)
-        music.playTone(523, music.beat(BeatFraction.Breve))
-        basic.showLeds(`
-            # # # # #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            . # . # .
-            # . . . #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # . . . #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            `)
-        basic.setLedColor(0x00ff00)
-        music.playMelody("E B C5 A B G A F ", 120)
-        music.playMelody("E D G F B A C5 B ", 120)
-        basic.showLeds(`
-            # # # # #
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            . . . # .
-            # . # . .
-            . # . . .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # . # . .
-            . # . . .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            . # . . .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-    }
-    basic.turnRgbLedOff()
+	
 })
 input.onButtonPressed(Button.B, function () {
-    if (randint(1, 100) == 2) {
+    if (randint(1, 10) == 2) {
         basic.showLeds(`
             # . . . #
             . # . # .
