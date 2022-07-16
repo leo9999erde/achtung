6ee86706b14f3feadf8fe51ef2c151b13fe52f6e
leo9999erde <96633138+leo9999erde@users.noreply.github.com>
2022-07-16 15:48:07 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    if (randint(1, 4) == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.setLedColor(0xff0000)
+        music.playMelody("C - C - C - D D ", 200)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+        basic.setLedColor(0x00ff00)
+        music.playMelody("E B C5 A B G A F ", 120)
+    }
+})
+basic.forever(function () {
+	
+})
