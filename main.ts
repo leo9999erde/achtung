454c41a530e43a58e3e042268a71e3736a059331
leo9999erde <96633138+leo9999erde@users.noreply.github.com>
2022-07-19 13:59:28 +0200

    neues spiel

--- main.ts
+++ main.ts
@@ -144,7 +144,7 @@ input.onPinPressed(TouchPin.P0, function () {
     zähler += 1
 })
 input.onButtonPressed(Button.A, function () {
-    if (randint(1, 100) == 2) {
+    if (randint(1, 10) == 2) {
         basic.showLeds(`
             # . . . #
             . # . # .
@@ -289,150 +289,18 @@ input.onButtonPressed(Button.AB, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
-    block = 0
-    if (randint(1, 10) == 2) {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        basic.setLedColor(0xff0000)
-        music.playMelody("C - C - C - D D ", 500)
-        music.playTone(523, music.beat(BeatFraction.Breve))
-        basic.showLeds(`
-            # # # # #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            . # . # .
-            # . . . #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # . . . #
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.clearScreen()
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . # . # .
-            # # # # #
-            # # # # #
-            . # # # .
-            . . # . .
-            `)
-        basic.showLeds(`
-            # . . # .
-            . # . . #
-            . . . . #
-            # . . # .
-            . # . . .
-            `)
-        basic.showLeds(`
-            . . . . #
-            # . . . .
-            . . . . .
-            . . . . #
-            # . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        reset += 1
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            `)
+    if (randint(1, 2) == 1) {
         basic.setLedColor(0x00ff00)
-        music.playMelody("E B C5 A B G A F ", 500)
-        music.playMelody("E D G F B A C5 B ", 500)
-        basic.showLeds(`
-            # # # # #
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            . . . # .
-            # . # . .
-            . # . . .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # . # . .
-            . # . . .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            . # . . .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+        basic.showString("Ja")
+    } else {
+        basic.setLedColor(0xff0000)
+        basic.showString("Nein")
     }
     basic.turnRgbLedOff()
-    block += 1
-    zähler += 1
 })
 basic.forever(function () {
     if (reset) {
+        music.stopMelody(MelodyStopOptions.All)
         basic.showNumber(zähler)
         basic.pause(2000)
         reset = 0
